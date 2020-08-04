65937ef35caaeb7a57bd969833e4b5bee618f87c
Jesus Flores-Padilla <jflorespadilla@nevada.unr.edu>
2020-08-03 21:11:32 -0700

    Did some projection matrix practice. Nothing really changed.

--- OpenGL/src/main.cpp
+++ OpenGL/src/main.cpp
@@ -29,7 +29,7 @@ int main(void) {
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
     /* Create a windowed mode window and its OpenGL context */
-    window = glfwCreateWindow(640, 480, "OpenGL", NULL, NULL);
+    window = glfwCreateWindow(960, 540, "OpenGL", NULL, NULL);
     if (!window) {
         glfwTerminate();
         return -1;
