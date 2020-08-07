afe16fd08470be18d31d97fabadf4a1b928eb062
Jesus Flores-Padilla <jflorespadilla@nevada.unr.edu>
2020-08-06 23:50:28 -0700

    Finished up imgui implementation and integration. Can now slide my image accross the screen.

--- OpenGL/src/main.cpp
+++ OpenGL/src/main.cpp
@@ -95,13 +95,9 @@ int main(void) {
 
         glm::mat4 proj = glm::ortho(-2.0f, 2.0f, -1.5f, 1.5f, -1.0f, 1.0f);
         glm::mat4 view = glm::translate(glm::mat4(1.0f), glm::vec3(-1.0f, 0.0f, 0.0f));
-        glm::mat4 model = glm::translate(glm::mat4(1.0f), glm::vec3(0.50f, 0.50f, 0.0f));
-
-        glm::mat4 mvp = proj * view * model;
 
         Shader shader("res/shaders/basic.shader");
         shader.bind();
-        shader.setUniformMat4f("u_MVP", mvp);
 
         Texture texture("res/textures/rainbow.png");
         texture.bind();
@@ -119,6 +115,8 @@ int main(void) {
         ImGui_ImplOpenGL3_Init("#version 330");
         ImGui::StyleColorsDark();
 
+        glm::vec3 translation(0.50f, 0.50f, 0.0f);
+
         bool show_demo_window = true;
         bool show_another_window = false;
         ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
@@ -132,31 +130,19 @@ int main(void) {
             ImGui_ImplGlfw_NewFrame();
             ImGui::NewFrame();
 
+            glm::mat4 model = glm::translate(glm::mat4(1.0f), translation);
+            glm::mat4 mvp = proj * view * model;
+
             shader.bind();
             //shader.setUniform4f("u_Color", r, g, b, 1.0f);
+            shader.setUniformMat4f("u_MVP", mvp);
 
             renderer.draw(va, ib, shader);
 
             {
-                static float f = 0.0f;
-                static int counter = 0;
-
-                ImGui::Begin("Dope side window");                          // Create a window called "Hello, world!" and append into it.
-
-                ImGui::Text("This is some useful text.");               // Display some text (you can use a format strings too)
-                ImGui::Checkbox("Demo Window", &show_demo_window);      // Edit bools storing our window open/close state
-                ImGui::Checkbox("Another Window", &show_another_window);
-
-                ImGui::SliderFloat("float", &f, 0.0f, 1.0f);            // Edit 1 float using a slider from 0.0f to 1.0f
-                ImGui::ColorEdit3("clear color", (float*)&clear_color); // Edit 3 floats representing a color
-
-                if (ImGui::Button("Button"))                            // Buttons return true when clicked (most widgets return true when edited/activated)
-                    counter++;
-                ImGui::SameLine();
-                ImGui::Text("counter = %d", counter);
-
+                ImGui::SliderFloat3("Translation", &translation.x, 0.0f, 1.0f);
                 ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
-                ImGui::End();
+
             }
 
             ImGui::Render();
