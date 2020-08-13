6da9a070e6e6fc92ae3b4c97e53f9ab3972b8a80
Jesus Flores-Padilla <jflorespadilla@nevada.unr.edu>
2020-08-12 23:36:47 -0700

    Implemented a simple test framework and produced a test.

--- OpenGL/src/main.cpp
+++ OpenGL/src/main.cpp
@@ -20,6 +20,7 @@
 #include "imgui/imgui.h"
 #include "imgui/imgui_impl_glfw.h"
 #include "imgui/imgui_impl_opengl3.h"
+#include "tests/testClearColor.h"
 
 int main(void) {
     GLFWwindow* window;
@@ -52,63 +53,8 @@ int main(void) {
 
     std::cout << glGetString(GL_VERSION) << std::endl;
     {
-        float positions[] = {
-            -0.5f, -0.5f, 0.0f, 0.0f,
-             0.5f, -0.5f, 1.0f, 0.0f,
-             0.5f,  0.5f, 1.0f, 1.0f,
-            -0.5f,  0.5f, 0.0f, 1.0f
-        };
-
-        unsigned int indicies[] = {
-            0, 1, 2,
-            2, 3, 0
-        };
-
-        /* Learned a bit about these functions and how they work under the hood
-           I should consider reviewing these functions on my own to let the info sink in */
         GLCall(glEnable(GL_BLEND));
         GLCall(glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA));
-
-        // Vertex array buffer - explicitly creating one
-        unsigned int vao;
-        GLCall(glGenVertexArrays(1, &vao));
-        GLCall(glBindVertexArray(vao));
-
-        /*
-           With the vertex arry buffer created, the next few buffers
-           are tied to the vao. This sets up the layout of the vao and can be bound
-           right before a draw call. This makes it easy to use multiple vertex array buffers
-           on different pieces of geometry.
-
-           Note - using a single vao and setting everything there is also a valid
-           way of doing things.
-        */
-        VertexArray va;
-        // Create an ID for a buffer
-        VertexBuffer vb(positions, 4 * 4 * sizeof(float));
-
-        VertexBufferLayout layout;
-        layout.push<float>(2);
-        layout.push<float>(2);
-        va.addBuffer(vb, layout);
-
-        // Index buffers
-        IndexBuffer ib(indicies, 6);
-
-        glm::mat4 proj = glm::ortho(-2.0f, 2.0f, -1.5f, 1.5f, -1.0f, 1.0f);
-        glm::mat4 view = glm::translate(glm::mat4(1.0f), glm::vec3(0.0f, 0.0f, 0.0f));
-
-        Shader shader("res/shaders/basic.shader");
-        shader.bind();
-
-        Texture texture("res/textures/rainbow.png");
-        texture.bind();
-        shader.setUniform1i("u_Texture", 0);
-
-        va.unbind();
-        vb.unbind();
-        ib.unbind();
-        shader.unbind();
 
         Renderer renderer;
 
@@ -117,50 +63,26 @@ int main(void) {
         ImGui_ImplOpenGL3_Init("#version 330");
         ImGui::StyleColorsDark();
 
-        glm::vec3 translationA(0.50f, 0.50f, 0.0f);
-        glm::vec3 translationB(1.0f, 1.0f, 0.0f);
-
         bool show_demo_window = true;
         bool show_another_window = false;
-        ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
 
-        /* Loop until the user closes the window */
+        test::TestClearColor test;
+
         while (!glfwWindowShouldClose(window)) {
-            /* Render here */
             renderer.clear();
+
+            test.onUpdate(0.0f);
+            test.onRender();
 
             ImGui_ImplOpenGL3_NewFrame();
             ImGui_ImplGlfw_NewFrame();
             ImGui::NewFrame();
-
-            shader.bind();
-            {
-                glm::mat4 model = glm::translate(glm::mat4(1.0f), translationA);
-                glm::mat4 mvp = proj * view * model;
-                shader.setUniformMat4f("u_MVP", mvp);
-                renderer.draw(va, ib, shader);
-            }
-
-            {
-                glm::mat4 model = glm::translate(glm::mat4(1.0f), translationB);
-                glm::mat4 mvp = proj * view * model;
-                shader.setUniformMat4f("u_MVP", mvp);
-                renderer.draw(va, ib, shader);
-            }
-
-            {
-                ImGui::SliderFloat3("Translation A", &translationA.x, -1.0f, 1.0f);
-                ImGui::SliderFloat3("Translation B", &translationB.x, -1.0f, 1.0f);
-                ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
-            }
-
+            test.onImguiRender();
             ImGui::Render();
             ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
 
-            /* Swap front and back buffers */
             glfwSwapBuffers(window);
 
-            /* Poll for and process events */
             glfwPollEvents();
         }
     }
---
+++ OpenGL/src/tests/testClearColor.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include "test.h"
+
+namespace test {
+	class TestClearColor : public Test {
+	public:
+		TestClearColor();
+		~TestClearColor();
+
+		void onUpdate(float deltaTime) override;
+		void onRender() override;
+		void onImguiRender() override;
+
+	private:
+		float m_ClearColor[4];
+	};
+}
