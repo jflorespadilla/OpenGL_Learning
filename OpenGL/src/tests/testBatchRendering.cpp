9728093a8e250ee33e33f8c706ac26b56cb86697
Jesus Flores-Padilla <jflorespadilla@nevada.unr.edu>
2020-09-22 22:29:51 -0700

    Just put in the basics. Still hasingout a good code sequence.

--- OpenGL/src/tests/testBatchRendering.cpp
+++ OpenGL/src/tests/testBatchRendering.cpp
@@ -7,7 +7,8 @@ namespace test {
 		// Initialize all relevant arrays of positions, indicies, and verticies
 
 		// Enable blending using OpenGL call
-
+		GLCall(glEnable(GL_BLEND));
+		GLCall(glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA));
 		// Pass information to buffers
 
 		// Bind buffers
@@ -24,7 +25,8 @@ namespace test {
 	}
 
 	void TestBatchRendering::onRender() {
-
+		GLCall(glClearColor(0.0f, 0.0f, 0.0f, 1.0f));
+		GLCall(glClear(GL_COLOR_BUFFER_BIT));
 	}
 
 	void TestBatchRendering::onImguiRender() {
