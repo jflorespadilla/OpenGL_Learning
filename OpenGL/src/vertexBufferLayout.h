4c257dee5cb2ba9d2dca4142e1d8be2646b2cbdf
Jesus Flores-Padilla <jflorespadilla@nevada.unr.edu>
2020-11-19 00:00:16 -0800

    Research

--- OpenGL/src/vertexBufferLayout.h
+++ OpenGL/src/vertexBufferLayout.h
@@ -10,9 +10,9 @@ struct VertexBufferElement {
 	static unsigned int getSizeOfType(unsigned int type) {
 		switch (type)
 		{
-		case GL_FLOAT:			return 4;
-		case GL_UNSIGNED_INT:	return 4;
-		case GL_UNSIGNED_BYTE:	return 1;
+			case GL_FLOAT:			return 4;
+			case GL_UNSIGNED_INT:	return 4;
+			case GL_UNSIGNED_BYTE:	return 1;
 		}
 		ASSERT(false);
 		return 0;
